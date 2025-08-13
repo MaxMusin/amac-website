4c411eed19bafe8061df926d583f413f55dc9dc0
MaxMusin <hello@maximemusin.me>
2025-08-14 01:17:46 +0200

    style: center hero section with flex justify-center class

--- src/views/landing/components/HeroSection.tsx
+++ src/views/landing/components/HeroSection.tsx
@@ -2,7 +2,10 @@ import Image from 'next/image';
 
 const HeroSection = () => {
   return (
-    <section id="hero" className="section-padding pb-0 px-0 relative bg-foreground overflow-hidden">
+    <section
+      id="hero"
+      className="section-padding pb-0 px-0 relative bg-foreground overflow-hidden flex justify-center"
+    >
       <Image src="/images/hero.png" alt="Hero" width={1920} height={1080} />
     </section>
   );
